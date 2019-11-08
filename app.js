249b5f0c7c2f7007ca40d4ac54417946a8dfc4dc
Jhon <jhon@MacBook-Pro-de-Jhon.local>
2019-11-08 07:13:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+
+    el: '#app',
+    data: {
+        titulo: 'List with Vue',
+        tareas: [],
+        nuevaTarea: ''
+    },
+    method:{
+        agregarTarea: function(){
+            this.tarea.push({
+                nombre: this.nuevaTarea,
+                estado: false
+            });
+            this.nuevaTarea = '';
+            localStorage.setItem('do-list', JSON.stringify(this.tareas));
+        },
+        editarTarea: function(index){
+            this.tareas[index].estado = true;
+            localStorage.setItem('do-list'.JSON.stringify(this.tareas));
+        },
+        eliminar(index){
+            this.tareas.splice(index, 1);
+            localStorage.setItem('do-list'.JSON.stringify(this.tareas));
+        }
+    },
+    created: function(){
+        let datosDB = JSON.parse(localStorage.getItem('do-list'));
+        if(datosDB === null){
+            this.tareas = [];
+        }else{
+            this.tareas = datosDB;
+        }
+    }
+})
